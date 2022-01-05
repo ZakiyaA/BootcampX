f0f5f57cd19a0076d04076cb156868ccfaf70e9d
ZakiyaAlfughi <47117848+ZakiyaA@users.noreply.github.com>
2022-01-05 00:48:32 -0500

    Update Cohorts_total_assignment_duration.sql

--- 2_queries_joins/Cohorts_total_assignment_duration.sql
+++ 2_queries_joins/Cohorts_total_assignment_duration.sql
@@ -1,7 +1,6 @@
 SELECT SUM(assignment_submissions.duration) As total_duration 
 FROM assignment_submissions
-JOIN students 
-ON assignment_submissions.student_id = students.id 
+JOIN students ON assignment_submissions.student_id = students.id 
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name = 'FEB12';
 
