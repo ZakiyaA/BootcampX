3881ff155305d3b9fae210edf7b145d9cdcc9a92
ZakiyaA <zakiyaalfughi@gmail.com>
2022-01-07 04:52:31 +0000

    Add Parameterized Query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,15 +8,17 @@ const pool = new Pool({
 });
 
 const cohort_name = process.argv[2];
-pool.query(`
+const values = [`%${cohort_name}%`];
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort 
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id 
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id 
-WHERE cohorts.name  LIKE '%${cohort_name}%'
+WHERE cohorts.name  LIKE $1
 ORDER BY teacher;
-`)
+`
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort} : ${user.teacher}`);
